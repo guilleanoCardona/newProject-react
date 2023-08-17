13f7dbe3babadc1b1ae9b4017635ea1e78ae0a57
guilleano <guilleano@gmail.com>
2023-08-16 21:51:02 -0500

    update state

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,36 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
   // null es que no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    const checkWinner = (boardToCheck) => {
+      for (const combo of WINNER_COMBOS) {
+        const [a, b, c] = combo
+        if (boardToCheck[a] &&
+          boardToCheck[b] === boardToCheck[b] &&
+          boardToCheck[a] === boardToCheck[c]) {
+          return boardToCheck[a]
+        }
+      }
+      return null
+    }
+  }
 
   const updateBoard = (index) => {
     // No actualizar esta posicion
@@ -38,6 +63,11 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // verificamos si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
   //const [count, setCount] = useState(0)
 
